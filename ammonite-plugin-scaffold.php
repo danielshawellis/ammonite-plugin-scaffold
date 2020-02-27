052a0b02a8a58cebf71d4b24cc34e77ce9ce0567
Daniel Ellis <danielshawellis@gmail.com>
2020-02-26 19:28:21 -0600

    enqueue styles and scripts

--- ammonite-plugin-scaffold.php
+++ ammonite-plugin-scaffold.php
@@ -19,7 +19,8 @@
   * -Fill out plugin header information
   * -Search and replace container class name ('Ammonite_Plugin_Scaffold') in this file
   * -Rename root plugin directory and ammonite-plugin-scaffold.php to match
-  * -Remove example dunctions
+  * -Remove unecessary example functions
+  * -Rename stylesheet and script in both this file (handle and path) and their location if they are being used
   */
 
 /*
@@ -38,8 +39,25 @@ if ( !class_exists( 'Ammonite_Plugin_Scaffold' ) ) {
     public static function example_function() {
 
     }
+
+    public static function example_register_styles_and_scripts() {
+      add_action( 'wp_enqueue_scripts', function() {
+        // Remove jQuery dependency if unnecessary
+        wp_register_script( 'ammonite-plugin-scaffold-script', plugins_url('assets/js/ammonite-plugin-scaffold.js', __FILE__ ), array('jquery'), '1.0.0', true );
+        wp_register_style( 'ammonite-plugin-scaffold-styles', plugins_url('assets/css/ammonite-plugin-scaffold.css', __FILE__ ), array(), '1.0.0', 'all' );
+      } );
+    }
+
+    public static function example_enqueue_styles_and_scripts() {
+      add_action( 'wp_enqueue_scripts', function() {
+        wp_enqueue_script( 'ammonite-plugin-scaffold-script' );
+        wp_enqueue_style( 'ammonite-plugin-scaffold-styles' );
+      } );
+    }
   }
 
   // Call methods on load here
   Ammonite_Plugin_Scaffold::example_function();
+  Ammonite_Plugin_Scaffold::example_register_styles_and_scripts();
+  Ammonite_Plugin_Scaffold::example_enqueue_styles_and_scripts();
 }
