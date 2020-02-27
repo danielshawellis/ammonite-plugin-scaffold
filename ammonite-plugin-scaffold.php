f10b82c2b14d3a40503beac92ac1ccfcb4da9f1f
Daniel Ellis <danielshawellis@gmail.com>
2020-02-26 19:08:17 -0600

    add headers, notes, and basic security

---
+++ ammonite-plugin-scaffold.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:       Ammonite Plugin Scaffold
+ * Plugin URI:
+ * Description:
+ * Version:           1.0.0
+ * Requires at least:
+ * Requires PHP:
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:
+ * Domain Path:
+ */
+
+ /**
+  * GETTING STARTED WITH THIS SCAFFOLD
+  * -Fill out plugin header information
+  * -Search and replace container class name in this file
+  * -Rename root plugin directory and ammonite-plugin-scaffold.php to match
+  */
+
+/*
+  Basic Security
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+  die;
+}
